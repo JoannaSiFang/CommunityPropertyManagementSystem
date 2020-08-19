6bd902210f5b83254be26de063eca6736e2b1a1c
JoannaSiFang <sifang615@gmail.com>
2020-08-18 21:59:58 -0700

    Create project base

---
+++ src/main/java/com/laioffer/communitypropertymanagementsystem/CommunitypropertymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.communitypropertymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitypropertymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitypropertymanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laioffer/communitypropertymanagementsystem/CommunitypropertymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.communitypropertymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitypropertymanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
