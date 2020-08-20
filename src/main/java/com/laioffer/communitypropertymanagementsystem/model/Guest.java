5657384a5e042f8e044c69d87feb6fa129c4b206
elanepu <58889674+elanepu@users.noreply.github.com>
2020-08-20 07:34:24 -0400

    create guest table using spring boot starter project option: Spring Data JPA with Hibernate(spring-boot-starter-data-jpa)

---
+++ src/main/java/com/laioffer/communitypropertymanagementsystem/model/Guest.java
@@ -0,0 +1,78 @@
+package com.laioffer.communitypropertymanagementsystem.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Guest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long guestID;
+
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private String phoneNumber;
+	private String message; 
+	private LocalDateTime time;
+
+	public Long getID() {
+		return guestID;
+	}
+	
+	public void setID(Long guestID) {
+		this.guestID = guestID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+}
