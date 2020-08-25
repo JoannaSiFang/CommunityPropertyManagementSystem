22074692038c194da17a5dbe0b423202a632f7f9
huazhang <zhanghua198936@gmail.com>
2020-08-24 19:03:51 -0700

    added profile and changepassword method

--- src/main/java/com/laioffer/communitypropertymanagementsystem/controller/UserController.java
+++ src/main/java/com/laioffer/communitypropertymanagementsystem/controller/UserController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Controller
 public class UserController {
@@ -29,4 +29,25 @@ public class UserController {
     List<User> getAllUsers() {
         return userService.getAllUser();
     }
+
+    @GetMapping(path="/profile")
+    public @ResponseBody
+    User getUserById(@RequestParam(value = "userId") Integer userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping(path="/changepassword")
+    public ResponseEntity changePassword(@RequestBody Map<String, String> params) {
+        try {
+            Integer userId = Integer.valueOf(params.get("User name"));
+            String newPassword = params.get("new Password");
+            User user = userService.getUserById(userId);
+            user.setPassword(newPassword);
+            userService.saveUser(user);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (NoSuchElementException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found", e);
+        }
+    }
+
 }
